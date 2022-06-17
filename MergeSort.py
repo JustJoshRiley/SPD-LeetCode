8d5fe4bb297747d5b811538af8c7ac1606c16a39
justjoshriley <joshriley316@gmail.com>
2022-06-16 18:54:51 -0700

    finished merge sort

--- MergeSort.py
+++ MergeSort.py
@@ -3,20 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p0 = 0
-        p2 = len(nums) - 1
+        red = 0
+        white = len(nums) - 1
         
         i = 0
         count = 0
         while count < len(nums):
             if nums[i] == 0:
-                nums[i], nums[p0] = nums[p0], nums[i]
-                p0 += 1
+                nums[i], nums[red] = nums[red], nums[i]
+                red += 1
                 i += 1
             elif nums[i] == 1:
                 i += 1
             else:
-                nums[i], nums[p2] = nums[p2], nums[i]
-                p2 -= 1
+                nums[i], nums[white] = nums[white], nums[i]
+                white -= 1
             count += 1
             
